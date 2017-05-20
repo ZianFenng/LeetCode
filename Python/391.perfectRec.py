f791cde35e294aa3ff24a92857837e1de23d8671
ZianFenng <LeonGong@leongong-2.local>
2017-05-20 15:50:18 -0400

    A faster version of perfect rectangle is updated
    Use a two dimensional array to record if a space is occupied or not to judge if overlapping happens
    
    if no overlapping & area matches, then perfect rectangle it is
    
    the problem here is if the input possible perfect rectangle is too large, then too much memory will be used

--- Python/391.perfectRec.py
+++ Python/391.perfectRec.py
@@ -24,53 +24,7 @@ class Solution(object):
             if curRec[3] > maxCor[3]:
                 maxCor[3] = curRec[3]
         return maxCor
-        
-    def isAllOverlapping(self, rectangles):
-        """
-        :type rectangles: List[List[int]]
-        :rtype: bool
-        """
-        numRec = len(rectangles)
-        for i in range(0,numRec-1):
-            recI = rectangles[i]
-            for j in range(i+1,numRec):
-                recJ = rectangles[j]
-                if self.isTwoOverlapping(recI, recJ):
-                    return True
-        return False
-                    
-    def isTwoOverlapping(self, rec1, rec2):
-        """
-        :type rec1: List[int]
-        :type rec2: List[int]
-        :rtype: bool
-        """
-        # Condition for non-overlapping
-        # if ((rec1[2] <= rec2[0] or rec1[3] <= rec2[1]) or (rec2[2] <= rec1[0] or rec2[3] <= rec1[1])):
-            # return False
-        # else:
-            # return True
-        # Condition for overlapping
-        # if (rec1[2] > rec2[0] and rec1[3] > rec2[1]) or (rec2[2] > rec1[0] and rec2[3] > rec1[1]):
-        #     return True
-        # else:
-        #     return False
-        # if rec1[3] > rec2[1]:
-        #     if rec1[1] >= rec2[3]:
-        #         return False
-        #     elif rec1[2] <= rec2[0] or rec2[2] <= rec1[0]:
-        #         return False
-        #     else:
-        #         return True
-        # else:
-        #     return False
-        
-        # if 1.left >= 2.right or 2.left >= 1.right or 1.bottom >= 2.up or 2.bottom >= 1.up
-        if (rec1[0] >= rec2[2] or rec2[0] >= rec1[2] or rec1[1] >= rec2[3] or rec2[1] >= rec1[3]):
-            return False
-        else:
-            return True
-    
+
     def calArea(self, rectangle):
         """
         :type rectangle: List[int]
@@ -80,24 +34,60 @@ class Solution(object):
         latitude  = rectangle[3] - rectangle[1]
         area = longitude * latitude
         return area
+
+    def addRec(self, perfectRec, newRec, L, B):
+        """
+        :type perfectRec:   List[List[int]]
+        :type newRec:       List[int]
+        :type L:            int
+        :type B:            int
+        :rtype:             bool
+        """
+        width = newRec[2] - newRec[0]
+        height = newRec[3] - newRec[1]
         
+        startHori  = newRec[0] - L
+        startVerti = newRec[1] - B
+        
+        for i in range(startHori, startHori + width):
+            for j in range(startVerti, startVerti + height):
+                if perfectRec[j][i]:
+                    return False # overlapped
+                else:
+                    perfectRec[j][i] = True
+                    
+        return True # Non overlapping
+
     def isRectangleCover(self, rectangles):
         """
         :type rectangles: List[List[int]]
         :rtype: bool
         """
         # [left, bottom, right , up]
+        # Possible perfect rec
+        maxRec = self.findMaxCoordinate(rectangles)
+        
+        # width and height of possible perfect rec
+        width = maxRec[2] - maxRec[0]
+        height  = maxRec[3] - maxRec[1]
+        maxArea = width * height
+        
+        # Coordinate of the origin of the perfect rec
+        originL = maxRec[0]
+        originB = maxRec[1]
+        
+        # Matrix to check overlapping. 0 -> nothing yet,  1 -> occupied
+        pfctRec = [ [False]*width for i in range(height) ]        
         numRec = len(rectangles)
-        if self.isAllOverlapping(rectangles):
-            return False
+        
+        allArea = 0
+        for i in range(numRec):
+            curRec = rectangles[i]
+            allArea += self.calArea(curRec)
+            if not self.addRec(pfctRec, curRec, originL, originB):
+                return False # overlapped, so not perfect rec
+       
+        if allArea == maxArea:
+            return True
         else:
-            maxRec = self.findMaxCoordinate(rectangles)
-            maxArea = self.calArea(maxRec)
-            allRecArea = 0
-            for i in range(0, numRec):
-                curRec = rectangles[i]
-                allRecArea += self.calArea(curRec)
-            if maxArea == allRecArea:
-                return True
-            else:
-                return False
+            return False
